a26865f55a4fccd2da27f05323ba5efa1cd8332c
Rianna1836 <160125020+Rianna1836@users.noreply.github.com>
2024-03-28 12:36:38 +0300

    доделать

--- app.js
+++ app.js
@@ -1,147 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const API_KEY_BOT = '6584920536:AAHwdvrjktGzaHWQexgar9Qq5T9sV81Y1iU';
-
-const bot = new TelegramBot(API_KEY_BOT, {
-    polling: true
-})
-const commands = [
-    {
-        command: 'start',
-        description: 'запуск'
-    },
-    {
-        command: 'keyb',
-        description: 'клавиатура'
-    },
-    {
-        command: 'help',
-        description: 'помощь'
-    },
-    {
-        command: 'my',
-        description: 'собрать самому'
-    },
-    {
-        command: 'code',
-        description: 'ввести код понравишегося букета'
-    }
-
-]
-bot.setMyCommands(commands)
-let latest = []
-function handleBackButton(word, latest) {
-    for (let i = 0; i<latest.length; i++) {
-        if(word !== latest[i]) {
-            latest.push(word)
-        }
-    }
+bot.sendMessage(chatId, 'Выберите цвет букета:', colorKeyboard); 
+        } else { 
+            bot.sendMessage(chatId, 'Подтвердите или отмените заказ:', confirmationKeyboard); 
+        } 
+    } 
+     
+    if (currentState === '') { 
+        bot.sendMessage(chatId, 'Добро пожаловать в наш магазин цветов! Чтобы сделать заказ, выберите цвет букета:', colorKeyboard); 
+        currentState = 'color'; 
+    } else if (currentState === 'quantity') { 
+        if (messageText === 'Назад') { 
+            currentState = 'flowers'; 
+            bot.sendMessage(chatId, Выберите цветы для ${order.color} цвета:, getFlowersKeyboard(order.color)); 
+            console.log(chatId) 
+        } else if (["1", "3", "5"].includes(messageText)) { 
+            order.quantity = parseInt(messageText); 
+            currentState = 'confirmation'; // Переходим к подтверждению заказа 
+            bot.sendMessage(chatId, Подтвердите ваш заказ:\n${order.quantity} цветов ${order.flowers} ${order.color} цвета., confirmationKeyboard); 
+        } else { 
+            bot.sendMessage(chatId, "Пожалуйста, выберите количество цветов из предложенных вариантов.", quantityKeyboard); 
+        } 
+    } 
+}); 
+function getFlowersKeyboard(color) { 
+    if (color === 'Красный') { 
+        return redFlowersKeyboard; 
+    } else if (color === 'Желтый') { 
+        return yellowFlowersKeyboard; 
+    } else if (color === 'Синий') { 
+        return blueFlowersKeyboard; 
+    } 
 }
-bot.onText(/\/start/, async (msg) => {
-    try {
-        const userName = msg.chat.first_name;
-        let word = msg.text
-        handleBackButton()
-        await bot.sendMessage(msg.chat.id, "Здравствуйте!")
-        await bot.sendMessage(msg.chat.id, "Вы уже выбрали какой букет желаете заказать или хотите собрать свой неповторимый букет? \nЕсли вы хотите создать свой неповторимый букет то выбирайте собрать самому, а если есть тот что вам понравился, зайдите в меню и введите код букета.",
-            {
-                reply_markup: {
-                    keyboard: [
-                        ['Собрать самому'],
-                        ['Ввести код'],
-                    ], resize_keyboard: true
-                }
-            })
-    } catch (error) {
-        console.log(error)
-        await bot.sendMessage(msg.chat.id, "Ошибка")
-    }
-})
-
-bot.on('text', async msg => {
-
-    //const chatId = msg.chat.id;
-    //if(msg.chat.id ii msg.from.id ii msg.chat.id) {
-    //    bot.getChat().then()
-    //}
-
-    try {
-
-        let word = msg.text
-        switch (word) {
-            case '/my':
-            case '/keyb':
-            case '/code':
-            case 'Собрать самому':
-                handleBackButton(word, latest)
-
-                console.log(word)
-                break;
-            default:
-                console.log(word + ' ' + 'Не записано')
-        }
-        if (latest.length > 4) {
-            latest.unshift()
-        } else {
-            console.log(latest)
-        }
-        let lastindex = latest.length
-        if(latest.length <= 1) {
-            lastindex = 0;
-        }else {
-            lastindex = latest.length - 2
-        }
-        
-
-        switch (msg.text) { 
-            case '/my':
-            case 'Собрать самому':
-        await bot.sendMessage(msg.chat.id, "Давайте собирать.")
-        await bot.sendMessage(msg.chat.id, "Выберите цвет.", {
-            reply_markup: {
-                keyboard: [
-                    ['Розовый'],
-                    ['Сиреневый'],
-                    ['Красный'],
-                    ['Синий'],
-                    ['Зелёный'],
-                    ['Голубой'],
-                    ['Назад']
-                ], resize_keyboard: true
-            }
-        })
-        break;
-            case '/code':
-            case 'Ввести код':
-        const rk = {
-            reply_markup: {
-                remove_keyboard: true
-            }
-        }
-        await bot.sendMessage(msg.chat.id, "Введите код.", rk)
-        break;
-        case 'Назад':
-            const userId = msg.from.id
-            bot.processUpdate({message:{text: latest[lastindex],chat:{id:userId}}})
-            break;
-    }
-
-        console.log(msg);
-
-} catch (error) {
-    console.log(error)
-    await bot.sendMessage(msg.chat.id, "Ошибка")
-}
-})
-
-//start идет вопрос про срочность запоминает ответ
-// то что сейчас есть в старт без изменений
-//всё как есть, запоминает все
-//если выбрали ввести код поиск в базе, должен быть доступ к нему ещё и у админов
-//высылает клиенту заказ на подтверждение, если верно оплата и разшифрованое админам
-//после цвета идет выбор: сладкий или цветочный, какие цветы или (если выбрали сладкий) сладости по отдельности и зависит от выбора, далее их количество, если цветы
-//сохраненое показывает для проверки клиентом
-// на клавиатуре появляются команды, чтобы если что-то не так исправить и подтверждение вправильности
-// возможность ввести код купона и проверка его
-//далее перекидывает на оплату
-//после подтверждения скидывает админам все детали заказа
